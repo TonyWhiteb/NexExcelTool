4a1b8e41d4065a619ece6fc2320af1e273a97399
TonyWhiteb <tonybai2016@gmail.com>
2019-06-24 15:20:45 -0500

    ChangeButtonClass

--- BasicClass/Button.py
+++ BasicClass/Button.py
@@ -4,19 +4,23 @@ import wx
 
 class ButtonPanel(wx.Panel):
 
-    def __init__(self,parent = None, id = -1,ButtonName = None, onButtonHandlers = None):
+    def __init__(self,parent = None, id = -1,ButtonName_1 = None, onButtonHandlers_1 = None, ButtonName_2 = None, onButtonHandlers_2 = None):
 
         super(ButtonPanel, self).__init__(parent = parent , id = id)
         
         # pub.subscribe(self.OnListen, 'GetSelectCol')
 
-        listALL = wx.Button(self,-1,ButtonName)
+        Button_1 = wx.Button(self,-1,ButtonName_1)
+        Button_2 = wx.Button(self,-1,ButtonName_2)
 
-        listALL.Bind(wx.EVT_LEFT_DOWN, onButtonHandlers)
+        Button_1.Bind(wx.EVT_LEFT_DOWN, onButtonHandlers_1)
+        Button_2.Bind(wx.EVT_LEFT_DOWN, onButtonHandlers_2)
 
         btnPanel_innerHorzSzr = wx.BoxSizer( wx.HORIZONTAL )
         btnPanel_innerHorzSzr.AddStretchSpacer( prop=1 )
-        btnPanel_innerHorzSzr.Add(listALL)
+        btnPanel_innerHorzSzr.Add(Button_1)
+        btnPanel_innerHorzSzr.AddSpacer( 25 )
+        btnPanel_innerHorzSzr.Add(Button_2)
         btnPanel_innerHorzSzr.AddSpacer( 25 )
 
         btnPanel_innerHorzSzr.AddStretchSpacer( prop=1 )
